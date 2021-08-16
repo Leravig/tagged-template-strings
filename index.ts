1fe9154eeed713b5576c8ea3ee51d78b2f70f1f3
David Owen Alterauge <kontakt@davidowen.de>
2021-08-16 18:35:13 +0200

    move changes to branch

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-export function tlh (strings: TemplateStringsArray, ...keys:number[]) {
-  return (function(...values:string[]) {
-    let dict: string = values[values.length - 1];
-    let result = [strings[0]];
-    keys.forEach(function(key, i) {
-      let value = Number.isInteger(key) ? values[key] : dict[key];
-      result.push(value, strings[i + 1]);
-    });
-    return result.join('');
-  });
-}
+export default function tlh () {}
